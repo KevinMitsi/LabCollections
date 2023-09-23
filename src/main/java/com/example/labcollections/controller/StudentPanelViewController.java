8c033da8842980c5c5937215654de7a4c22c0181
KevinMitsi <kevinandresgarciaaguirre@gmail.com>
2023-09-23 17:32:36 -0500

    Creados las controladores de las views

--- src/main/java/com/example/labcollections/controller/StudentPanelViewController.java
+++ src/main/java/com/example/labcollections/controller/StudentPanelViewController.java
@@ -2,31 +2,90 @@ package com.example.labcollections.controller;
 
 import com.example.labcollections.MainApplication;
 import com.example.labcollections.model.Estudiante;
+import com.example.labcollections.model.Libro;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableSet;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 
 public class StudentPanelViewController {
+    private ObservableSet<Libro> librosEstudiantesData = FXCollections.observableSet();
+    private ObservableSet<Libro> librosDisponiblesData = FXCollections.observableSet();
+    private ObservableList<Libro> librosEstudiantesList;
+    private ObservableList<Libro> librosDisponiblesList;
+    private Libro libroPropioSeleccionado = null;
+    private Libro libroDisponibleSeleccionado = null;
+
     MainApplication main;
     ModelFactoryController singleton = ModelFactoryController.getInstance();
     Estudiante estudianteLogeado;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public TableColumn<Libro, String> colNombre;
+    public TableColumn<Libro, String> colAutor;
+    public Label lblNombreEstudiante;
+    public TableColumn<Libro, String>  colNomreDispo;
+    public TableColumn<Libro, String>  colAutorDispo;
+    public Button btnCerrar;
+    public TableView<Libro> tableLibrosDisponibles;
+    public TableView<Libro>tableLibrosEstudiantes;
 
 
     public void setMain(MainApplication main, Estudiante estudiante) {
         this.main = main;
         estudianteLogeado = estudiante;
+        tableLibrosDisponibles.getItems().clear();
+        tableLibrosDisponibles.setItems(obtenerLibrosDisponibles());
+        tableLibrosEstudiantes.getItems().clear();
+        tableLibrosEstudiantes.setItems(obtenerLibrosEstudiantes());
     }
+
+    public void onDragSelection(MouseEvent mouseEvent) {
+        if (libroPropioSeleccionado==null){
+            Alerta.saltarAlertaError("No ha seleccionado ningún libro");
+        }
+        else {
+            main.abrirVistaPrestamoEstudiante(estudianteLogeado);
+        }
+
+    }
+
+    public void onCerrarButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onDragDisponible(MouseEvent mouseEvent) {
+    }
+    @FXML
+    void initialize(){
+        colAutor.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAutor()));
+        colNombre.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTitulo()));
+        tableLibrosEstudiantes.refresh();
+        colAutorDispo.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAutor()));
+        colNomreDispo.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTitulo()));
+        tableLibrosDisponibles.refresh();
+        tableLibrosEstudiantes.getSelectionModel().selectedItemProperty().addListener((obs,oldSelection, newSelection)->{
+            libroPropioSeleccionado = newSelection;
+        });
+        tableLibrosDisponibles.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection)->{
+            libroDisponibleSeleccionado = newSelection;
+        });
+    }
+
+    private ObservableList<Libro> obtenerLibrosEstudiantes(){
+       librosEstudiantesData.addAll(estudianteLogeado.getMisLibros());
+        librosEstudiantesList = FXCollections.observableArrayList(librosEstudiantesData);
+        return librosEstudiantesList;
+    }
+
+    private ObservableList<Libro> obtenerLibrosDisponibles(){
+        librosEstudiantesData.addAll(singleton.getLibros());
+        librosDisponiblesList= FXCollections.observableArrayList(librosDisponiblesData);
+        return librosDisponiblesList;
+    }
+
 }
--- src/main/java/com/example/labcollections/model/Estudiante.java
+++ src/main/java/com/example/labcollections/model/Estudiante.java
@@ -1,10 +1,17 @@
 package com.example.labcollections.model;
 
-import java.util.Objects;
+import com.example.labcollections.exception.LibroException;
+import com.example.labcollections.exception.PrestamoException;
+
+import java.util.*;
 
 public class Estudiante {
     private String id;
     private String nombre;
+
+    private Set<Libro> misLibros;
+
+    private Map<Integer, Prestamo> misPrestamos;
 
     public Estudiante() {
     }
@@ -12,6 +19,8 @@ public class Estudiante {
     public Estudiante(String id, String nombre) {
         this.id = id;
         this.nombre = nombre;
+        this.misLibros = new TreeSet<>();
+        this.misPrestamos = new HashMap<>();
     }
 
 
@@ -29,6 +38,14 @@ public class Estudiante {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public Map<Integer, Prestamo> getMisPrestamos() {
+        return misPrestamos;
+    }
+
+    public void setMisPrestamos(Map<Integer, Prestamo> misPrestamos) {
+        this.misPrestamos = misPrestamos;
     }
 
     @Override
@@ -49,6 +66,52 @@ public class Estudiante {
                 "id='" + id + '\'' +
                 ", nombre='" + nombre + '\'' +
                 '}';
+    }
+
+    public Set<Libro> getMisLibros() {
+        return misLibros;
+    }
+
+    public void setMisLibros(Set<Libro> misLibros) {
+        this.misLibros = misLibros;
+    }
+
+    public void agregarLibro(Libro libro) throws LibroException {
+        if (misLibros.contains(libro)){
+            throw new LibroException("Ya tienes este libro prestado");
+        }
+        else {
+            libro.setPrestado(true);
+            misLibros.add(libro);
+        }
+    }
+
+    public void devolverLibro(Libro libro) throws LibroException {
+        if(!misLibros.contains(libro)){
+            throw new LibroException("Este libro no lo tiene prestado");
+        }
+        else {
+            libro.setPrestado(false);
+            misLibros.remove(libro);
+        }
+    }
+
+    public void hacerPrestamo(Integer key, Prestamo prestamo) throws PrestamoException {
+        if(misPrestamos.containsValue(prestamo)){
+            throw new PrestamoException("No se pueden hacer préstamo repetidos");
+        }
+        else {
+            misPrestamos.put(key, prestamo);
+        }
+    }
+
+    public void finalizarPrestamo(Integer key, Prestamo prestamo) throws PrestamoException {
+        if (!misPrestamos.containsValue(prestamo)){
+            throw new PrestamoException("Este préstamo ya fue finalizado");
+        }
+        else {
+            misPrestamos.remove(key, prestamo);
+        }
     }
 
 
